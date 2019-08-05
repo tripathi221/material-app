2c368c2a77b777fab78e413d7d0377431ae65079
tripathi221 <laxmikant.tripathi6@gmail.com>
2019-08-05 22:40:26 +0530

    final setup

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var EMPLOYEE_COLLECTION = "employees";
-var DEPARTMENT_COLLECTION = "departments";
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -30,7 +33,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     });
 });
 
-// EMPLOYEES API ROUTES BELOW
+// CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -38,31 +41,31 @@ function handleError(res, reason, message, code) {
     res.status(code || 500).json({ "error": message });
 }
 
-/*  "/api/employee"
- *    GET: finds all employee
- *    POST: creates a new employee
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
  */
 
-app.get("/api/employee", function (req, res) {
-    db.collection(EMPLOYEE_COLLECTION).find({}).toArray(function (err, docs) {
+app.get("/api/contacts", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
-            handleError(res, err.message, "Failed to get employees.");
+            handleError(res, err.message, "Failed to get contacts.");
         } else {
             res.status(200).json(docs);
         }
     });
 });
 
-app.post("/api/employee", function (req, res) {
-    var newEmployee = req.body;
-    newEmployee.createDate = new Date();
+app.post("/api/contacts", function (req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
 
     if (!req.body.name) {
         handleError(res, "Invalid user input", "Must provide a name.", 400);
     } else {
-        db.collection(EMPLOYEE_COLLECTION).insertOne(newEmployee, function (err, doc) {
+        db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
             if (err) {
-                handleError(res, err.message, "Failed to add a new employee.");
+                handleError(res, err.message, "Failed to create new contact.");
             } else {
                 res.status(201).json(doc.ops[0]);
             }
@@ -70,17 +73,42 @@ app.post("/api/employee", function (req, res) {
     }
 });
 
-/*  "/api/employee/:id"
- *    GET: find employee by id
- *    PUT: update employee by id
- *    DELETE: deletes employee by id
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
  */
 
-app.get("/api/employee/:id", function (req, res) {
+app.get("/api/contacts/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contact");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
 });
 
-app.put("/api/employee/:id", function (req, res) {
+app.put("/api/contacts/:id", function (req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to update contact");
+        } else {
+            updateDoc._id = req.params.id;
+            res.status(200).json(updateDoc);
+        }
+    });
 });
 
-app.delete("/api/employee/:id", function (req, res) {
+app.delete("/api/contacts/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete contact");
+        } else {
+            res.status(200).json(req.params.id);
+        }
+    });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDetailsComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
